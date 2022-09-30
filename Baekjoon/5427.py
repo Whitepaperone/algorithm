8c73d5f934ddbcbc6ee9c88a5f2d9262530ff076
Whitepaperone <qorwldnjs100@gmail.com>
2022-09-30 17:46:14 +0900

    update

--- Baekjoon/5427.py
+++ Baekjoon/5427.py
@@ -1,70 +1,60 @@
 from collections import deque
+
+
 def bfs():
     global time
     while q:
         curY, curX, time = q.pop(0)
-        if time == 0:
-            visited[curY][curX] = '*'
         for di, dj in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
             nY = curY + di
             nX = curX + dj
-            if 0 <= nY < M + 2 and 0 <= nX < N + 2 and visited[nY][nX] != -1 and visited[nY][nX] == 0:
+            if 0 <= nY < M and 0 <= nX < N and arr[nY][nX] != -1 and arr[nY][nX] == 0:
                 q.append((nY, nX, time + 1))
-                visited[nY][nX] = time + 1
-    return time
+                arr[nY][nX] = time + 1
+    return
 
 
 def bfs2(y, x, time):
     q.append((y, x, time))
     while q:
         curY, curX, time = q.pop(0)
-        if time == 0:
-            visited[curY][curX] = '@'
         for di, dj in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
             nY = curY + di
             nX = curX + dj
-            if 0 <= nY < M + 2 and 0 <= nX < N + 2 and visited[nY][nX] != -1:
-                if type(visited[nY][nX]) == int:
-                    if visited[nY][nX] > time:
-                        q.append((nY, nX, time + 1))
-                        visited[nY][nX] = 10 * (time + 1)
-    return time
+            if 0 <= nY < M and 0 <= nX < N and arr[nY][nX] != -1 and arr[nY][nX] > time:
+                q.append((nY, nX, time + 1))
+                arr[nY][nX]=time+1
+            if 0>nY or nY >= M or 0>nX or nX >= N:
+                return time
+    return 'IMPOSSIBLE'
 
 
 TC = int(input())
 for test_case in range(1, TC + 1):
 
     N, M = map(int, input().split())
-    arr = [input() for i in range(M)]
-    visited = [[0] * (N + 2) for i in range(M + 2)]
+    arr = [list(input()) for i in range(M)]
     q = []
-    time = 0
+    time = 1
     for i in range(M):
         for j in range(N):
             if arr[i][j] == '*':
-                q.append((i + 1, j + 1, time))
+                arr[i][j] = 1
+                q.append((i, j, time))
             if arr[i][j] == '#':
-                visited[i + 1][j + 1] = -1
+                arr[i][j] = -1
             if arr[i][j] == '@':
-                a = i + 1
-                b = j + 1
+                arr[i][j] = 1
+                a = i
+                b = j
+            if arr[i][j] == '.':
+                arr[i][j] = 0
+    print(arr)
     bfs()
-    bfs2(a, b, 0)
-    minV = 999999
-    time = 0
-    for i in range(M + 2):
-        if i == 0 or i == M + 1:
-            for j in range(N + 2):
-                if visited[i][j]%10:
-                    time = visited[i][j]//10
-                    if time < minV:
-                        minV = time
-        else:
-            for j in [0, N + 1]:
-                if visited[i][j]%10:
-                    time = visited[i][j]//10
-                    if time < minV:
-                        minV = time
-    if time == 0:
-        minV = 'IMPOSSIBLE'
-    print(minV)
+    time = bfs2(a, b, 0)
+
+
+    if time != 'IMPOSSIBLE':
+        print(time+1)
+    else:
+        print(time)
