370ab24ad80eac0393843eb76aa13557584c3195
Whitepaperone <qorwldnjs100@gmail.com>
2022-11-15 22:34:03 +0900

    update

--- Baekjoon/3190.py
+++ Baekjoon/3190.py
@@ -1,9 +1,32 @@
+def go():
+    global res,dir
+    r,c=snake[-1]
+    res+=1
+    nr=r+dr[dir%4]
+    nc=c+dc[dir%4]
+    if (nr,nc) in snake:
+        return res
+    snake.append((nr,nc))
+    if arr[nr][nc]==0:
+        snake.pop(0)
 N=int(input())
 K=int(input())
 arr=[[0 for i in range(N)]for i in range(N)]
+snake=[]
+dr=[0,1,0,-1]
+dc=[1,0,-1,0]
+dir=4
+res=0
 for i in range(K):
     row,col=map(int,input().split())
     row-=1
     col-=1
+    arr[row][col]=1
 L=int(input())
-for i in 
+snake.append((0,0))
+for i in range(L):
+    X,C=map(str,input().split())
+    for i in range(int(X)):
+        go()
+    if C=='D':
+        
